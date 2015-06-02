63886dd093b0d3c60e153b344b72ef33fc1b9fec
drose379 <dylanrose60@gmail.com>
2015-06-01 22:32:59 -0400

    file not recieved from java

--- setData/albumDefaultInsert.php
+++ setData/albumDefaultInsert.php
@@ -8,7 +8,7 @@ class albumDefault {
 		$imageTempName = $_FILES["photo"]["tmp_name"];
 		$imageRealName = $_FILES["photo"]["name"];
 
-		var_dump($_FILES);
+		var_dump($_POST);
 
 		//get image resource from temp location
 		$imageResource = imagecreatefromjpeg($imageTempName);
