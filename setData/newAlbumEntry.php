c26f2346ffd5ca62c0a5dacebf22847f412dd525
drose379 <dylanrose60@gmail.com>
2015-06-06 13:25:41 -0400

    making sure all data is being sent with post

--- setData/newAlbumEntry.php
+++ setData/newAlbumEntry.php
@@ -8,21 +8,29 @@ class newAlbum {
 	private $subject;
 	private $lesson;
 	private $imageLocation;
+	private $albumName;
+	private $albumDesc;
+
+	//Need to grab album name and desc from post
 
 	public function run() {
 		$post = file_get_contents("php://input");
+		var_dump($post);
+		/*
 		$this->subject = $post["subject"];
 		$this->lesson = $post["lesson"];
 		$this->imageLocation = $post["imageLocation"];
+		$this->albumName = $post["albumName"];
+		$this->albumDesc = $post["albumDesc"];
 
 		$this->addLessonAlbum();
+		*/
 	}
 
 	public function addLessonAlbum() {
 		$connection = Connection::get();
-		if ($connection != null) {
-			echo "Connection is not null!";
-		}
+		$stmt = $connection->prepare("INSERT INTO lesson_albums (subject,lesson,album,default_photo,description) VALUES 
+			(:subject,:lesson,:albumName,:defaultPhoto,:description)");
 	}
 
 }
